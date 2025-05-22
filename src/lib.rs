1ecef06d00c8520dd976c5cd2002cd71f732d045
Iago Bozza <iagobozza@writeonlycode.io>
2025-05-22 12:49:30 -0300

    Move core logic from main.rs to lib.rs with error handling. Closes #5

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,19 @@
+use anyhow::Result;
 use clap::{Parser, Subcommand};
+use crossterm::{
+    cursor::MoveTo,
+    event::{read, Event, KeyCode, KeyEvent},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, Clear},
+};
+use std::{
+    sync::mpsc,
+    thread::{self},
+    time::Duration,
+};
+use timer::{Timer, TimerMessage};
+
+mod timer;
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
@@ -20,4 +35,78 @@ pub enum Commands {
 
     /// View the history of completed sessions
     Log,
+}
+
+pub fn run(config: Config) -> Result<()> {
+    let (tx, rx) = mpsc::channel::<TimerMessage>();
+
+    match config.command {
+        Commands::Start => {
+            let mut timer = Timer::new(Duration::from_secs(3), rx);
+
+            thread::spawn(move || {
+                timer.run();
+            });
+
+            let _ = tx.send(TimerMessage::Start);
+        }
+        Commands::Break => {
+            let mut timer = Timer::new(Duration::from_secs(300), rx);
+
+            thread::spawn(move || {
+                timer.run();
+            });
+
+            let _ = tx.send(TimerMessage::Start);
+        }
+        Commands::LongBreak => {
+            let mut timer = Timer::new(Duration::from_secs(900), rx);
+
+            thread::spawn(move || {
+                timer.run();
+            });
+
+            let _ = tx.send(TimerMessage::Start);
+        }
+        Commands::Log => {
+            todo!("Log!")
+        }
+    }
+
+    let _ = enable_raw_mode();
+
+    loop {
+        match read() {
+            Ok(Event::Key(KeyEvent {
+                code: KeyCode::Char('s'),
+                ..
+            })) => {
+                let _ = tx.send(TimerMessage::ToggleStop);
+            }
+            Ok(Event::Key(KeyEvent {
+                code: KeyCode::Char('p'),
+                ..
+            })) => {
+                let _ = tx.send(TimerMessage::TogglePause);
+            }
+            Ok(Event::Key(KeyEvent {
+                code: KeyCode::Char('q'),
+                ..
+            })) => {
+                break;
+            }
+            Err(error) => println!("{:?}", error),
+            _ => (),
+        }
+    }
+
+    let _ = execute!(
+        std::io::stdout(),
+        Clear(crossterm::terminal::ClearType::All),
+        MoveTo(0, 0),
+    );
+
+    let _ = disable_raw_mode();
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,10 @@
-use std::{
-    sync::mpsc,
-    thread::{self},
-    time::Duration,
-};
-
 use clap::Parser;
-use crossterm::{
-    cursor::MoveTo,
-    event::{read, Event, KeyCode, KeyEvent},
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, Clear},
-};
-use oxitime::{Commands::*, Config};
-use timer::{Timer, TimerMessage};
-
-mod timer;
+use oxitime::{run, Config};
 
 fn main() {
     let config = Config::parse();
-    let (tx, rx) = mpsc::channel::<TimerMessage>();
 
-    match config.command {
-        Start => {
-            let mut timer = Timer::new(Duration::from_secs(3), rx);
-
-            thread::spawn(move || {
-                timer.run();
-            });
-
-            let _ = tx.send(TimerMessage::Start);
-        }
-        Break => {
-            let mut timer = Timer::new(Duration::from_secs(300), rx);
-
-            thread::spawn(move || {
-                timer.run();
-            });
-
-            let _ = tx.send(TimerMessage::Start);
-        }
-        LongBreak => {
-            let mut timer = Timer::new(Duration::from_secs(900), rx);
-
-            thread::spawn(move || {
-                timer.run();
-            });
-
-            let _ = tx.send(TimerMessage::Start);
-        }
-        Log => {
-            todo!("Log!")
-        }
+    if let Err(error) = run(config) {
+        eprintln!("{}", error)
     }
-
-    let _ = enable_raw_mode();
-
-    loop {
-        match read() {
-            Ok(Event::Key(KeyEvent {
-                code: KeyCode::Char('s'),
-                ..
-            })) => {
-                let _ = tx.send(TimerMessage::ToggleStop);
-            }
-            Ok(Event::Key(KeyEvent {
-                code: KeyCode::Char('p'),
-                ..
-            })) => {
-                let _ = tx.send(TimerMessage::TogglePause);
-            }
-            Ok(Event::Key(KeyEvent {
-                code: KeyCode::Char('q'),
-                ..
-            })) => {
-                break;
-            }
-            Err(error) => println!("{:?}", error),
-            _ => (),
-        }
-    }
-
-    let _ = execute!(
-        std::io::stdout(),
-        Clear(crossterm::terminal::ClearType::All),
-        MoveTo(0, 0),
-    );
-
-    let _ = disable_raw_mode();
 }
